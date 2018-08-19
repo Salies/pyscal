4c8ed10faf0cbe74ac90bc3312f790399cfd1dcb
Salies <serezane@protonmail.com>
2018-08-19 12:09:45 -0300

    v2 w/tables

--- pyscal.py
+++ pyscal.py
@@ -1,6 +1,7 @@
 import math
+from terminaltables import AsciiTable
 
-size = 10 #change this to the ammount of lines you want to draw/calc
+size = 10 #change to the desired amount of lines (always remember that it starts with 0)
 triangle = []
 
 def factorial(x):
@@ -9,24 +10,21 @@ def factorial(x):
     else:
         return math.factorial(x)
 
-for n in range(size):
-    triangle.append([])
-    for p in range(n + 1):
-        triangle[n].append([n, p])
+def count(n, p):
+    return int(factorial(n) / (factorial(p) * factorial(n - p)))
 
-def draw():
-    for i in range(len(triangle)):
-        for x in range(len(triangle[i])):
-            print('%d ' % triangle[i][x][0], end=" ")
-        print('')
-        for y in range(len(triangle[i])):
-            print('%s%d ' % ((" " * (len(str(triangle[i][x][0])) - len(str(triangle[i][y][1])))), triangle[i][y][1]), end=" ")
-        print('\n')
+def draw(x, y):
+    return '%d\n%d' % (x, y)
 
-def calc():
-    for i in range(len(triangle)):
-        for t in range(len(triangle[i])):
-            print(int(factorial(triangle[i][t][0]) / (factorial(triangle[i][t][1]) * factorial(triangle[i][t][0] - triangle[i][t][1]))), end=" ") #n! / p!(n-p)!
-        print('')
+def main(size, func):
+    for n in range(size):
+        triangle.append([])
+        for p in range(n + 1):
+            triangle[n].append(func(n,p))
 
-calc() #change to draw() for the binomials
+main(size, count) #change to draw if you want the binomials
+
+table = AsciiTable(triangle)
+table.inner_heading_row_border = False
+table.inner_row_border = True
+print(table.table)
